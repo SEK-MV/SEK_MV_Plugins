8b883536a9ea5a662b53ab50ede53c36a2d8419b
SEK-MV <ste95ven@gmail.com>
2016-06-13 15:13:29 +0200

    Not fighting members will be behind your fighting members!
    Not fighting members will be behind your fighting members!

--- SEK_HiddenMembers.js
+++ SEK_HiddenMembers.js
@@ -10,11 +10,26 @@
 *@desc Not fighting members opacity (from 0 to 255). Default is 150.
 *@default 150
 *
+*@help
+* Plugin Commands:
+*
+* hm x	- 	changes opacity to x (from 0 to 255)
+* 			0 = invisible, 255 = as fighting members
 */
 
 var params=PluginManager.parameters('SEK_HiddenMembers');
 var opacità = Number(params['Opacity'] || 150);
-
+var aliashm = Game_Interpreter.prototype.pluginCommand;
+	Game_Interpreter.prototype.pluginCommand = function(command, args) {
+		aliashm.call(this, command, args);
+		if (command.toLowerCase() === "hm") 
+		{
+			if (args[0]>255) opacità=255;
+			else if (args[0]<0) opacità=0;
+			else opacità=Number(args[0]);
+		}
+	};
+	
 Spriteset_Battle.prototype.createActors = function() {
     this._actorSprites = [];
     for (var i = 0; i < 8; i++) {
@@ -51,7 +66,7 @@ Game_Party.prototype.hiddenMembers = function() {
 
 
 Sprite_Actor.prototype.setActorHome = function(index, add) {
-    this.setHome(600 + index * 32+(add?40:0), 280 + (add==1?(index-$gameParty.maxBattleMembers())*48:index*48));
+    this.setHome(600 + index * 32+(add?20:0), 280 + (add==1?(index-$gameParty.maxBattleMembers())*48:index*48));
 };
 
 Sprite_Actor.prototype.setBattler = function(battler, i,add) {
@@ -65,5 +80,5 @@ Sprite_Actor.prototype.setBattler = function(battler, i,add) {
         this.startEntryMotion();
         this._stateSprite.setup(battler);
     }
-    if (add==1) this.opacity =opacità;
+    if (add==1) this.opacity = opacità;
 };
